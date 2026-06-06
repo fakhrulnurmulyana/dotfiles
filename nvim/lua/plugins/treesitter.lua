7c70627346473a451e48467951916f6e1e5e0897
fakhrulnurmulyana <115722602+fakhrulnurmulyana@users.noreply.github.com>
2026-06-06 11:22:57 +0700

    fix(treesitter): re-configure treesitter & update lazy-lock

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,28 +5,14 @@ return {
 		build = ":TSUpdate",
 
 		config = function()
-			require("nvim-treesitter").setup {
-				ensure_installed = {
-					"lua",
-					"python",
-					"rust",
-					"c",
-					"bash",
-					"json",
-					"markdown",
-					"vim",
-					"vimdoc",
-				},
-
-				auto_install = true,
-
-				highlight = {
-					enable = true,
-				},
-
-				indent = {
-					enable = true,
-				},
+			require('nvim-treesitter').install{
+				"lua",
+				"python",
+				"rust",
+				"c",
+				"bash",
+				"json",
+				"markdown",
 			}
 		end,
 	},
